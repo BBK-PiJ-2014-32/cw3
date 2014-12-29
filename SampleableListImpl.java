e8db04506518f5d1e5624d00753d38e72a94f62d
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-29 13:00:45 +0000

    first commit

---
+++ SampleableListImpl.java
@@ -0,0 +1,14 @@
+/**
+ * A sampleable list can be sampled. 
+ * 
+ * @author PiJ
+ */
+public class SampleableListImpl implements SampleableList {
+	/**
+	 * Returns a list containing the first, third, fifth...
+	 * items of this list, or an empty list if the list is empty. 
+	 * 
+	 * @return a list containing the first, third, fifth... items of this list
+	 */
+	public SampleableList sample();
+}
