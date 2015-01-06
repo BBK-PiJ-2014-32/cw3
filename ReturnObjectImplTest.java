0d3ca8f283e4958369465bcc5c35f0a1d9a3bf9a
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-06 23:53:38 +0000

    initial basic testing

--- ReturnObjectImplTest.java
+++ ReturnObjectImplTest.java
@@ -1,5 +1,8 @@
 public class ReturnObjectImplTest {
 	
-	ReturnObject testObject = new ReturnObjectImpl(34);
-	
+	public static void main(String[] args){
+		ReturnObject testObject = new ReturnObjectImpl(34);
+		ErrorMessage msg = testObject.getError();
+		System.out.println(msg);
+	}
 }
