36dca158abfcc3f58c8982a379aa726dc3baf8fd
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-24 16:27:18 +0000

    rest test added and failed

--- FunctionalArrayListTest.java
+++ FunctionalArrayListTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 public class FunctionalArrayListTest {
 
+	/**
+	 * Head test.
+	 */
 	@Test
 	public void headTest() {
 		FunctionalArrayList myArrayList = new FunctionalArrayList();
@@ -13,7 +16,23 @@ public class FunctionalArrayListTest {
 		Object output = returnObj.getReturnValue();
 		Object expected = "A";
 		assertEquals(expected, output);
-		
+	}
+	
+	/**
+	 * Rest test.
+	 */
+	@Test
+	public void restTest() {
+		FunctionalArrayList myArrayList = new FunctionalArrayList();
+		myArrayList.add("a");
+		myArrayList.add("b");
+		myArrayList.add("c");
+		myArrayList.add("d");
+		myArrayList.add("e");
+		FunctionalArrayList newArrayList = myArrayList.rest();
+		int output = newArrayList.size();
+		int expected = 5;
+		assertEquals(expected, output);
 	}
 
 }
