fd7a6ed0dfea5be67ffabbd2333b9d0157718e7f
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-25 17:31:48 +0000

    Junit tests from stackImpl used and passed

---
+++ ImprovedStackImplTest.java
@@ -0,0 +1,74 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ImprovedStackImplTest {
+
+	@Test
+	public void stackImplListPushTest(){
+		List testLinkedList = new LinkedList();
+		ImprovedStackImpl testStack = new ImprovedStackImpl(testLinkedList);
+		testStack.push("a");
+		testStack.push("b");
+		testStack.push("c");
+		testStack.push("d");
+		testStack.push("e");
+		testStack.push("f");
+		ReturnObject returObj = testStack.top();
+		Object output = returObj.getReturnValue();
+		Object expected = "f";
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void stackImplListPopTest(){
+		List testLinkedList = new LinkedList();
+		ImprovedStackImpl testStack = new ImprovedStackImpl(testLinkedList);
+		testStack.push("a");
+		testStack.push("b");
+		testStack.push("c");
+		testStack.push("d");
+		testStack.push("e");
+		testStack.push("f");
+		testStack.pop();
+		testStack.pop();
+		testStack.pop();
+		ReturnObject returObj = testStack.pop();
+		Object output = returObj.getReturnValue();
+		Object expected = "c";
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void stackImplListSizeTest(){
+		List testLinkedList = new LinkedList();
+		ImprovedStackImpl testStack = new ImprovedStackImpl(testLinkedList);
+		testStack.push("a");
+		testStack.push("b");
+		testStack.push("c");
+		testStack.push("d");
+		testStack.push("e");
+		testStack.push("f");
+		int output = testStack.size();
+		int expected = 6;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void stackImplListIsEmptyTest(){
+		List testLinkedList = new LinkedList();
+		ImprovedStackImpl testStack = new ImprovedStackImpl(testLinkedList);
+		testStack.push("a");
+		testStack.push("b");
+		testStack.push("c");
+		testStack.push("d");
+		testStack.push("e");
+		testStack.push("f");
+		boolean output = testStack.isEmpty();
+		boolean expected = false;
+		assertEquals(expected, output);
+
+	}
+
+}
