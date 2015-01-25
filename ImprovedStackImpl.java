b2fb904c4e46a649779c615a36d74157a27b93f7
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-25 17:21:41 +0000

    ImprovedStackImpl added

--- ImprovedStackImpl.java
+++ ImprovedStackImpl.java
@@ -16,8 +16,11 @@
  */
 public class ImprovedStackImpl implements ImprovedStack {
 	
-	public ImprovedStackImpl(){
-
+	private List internalList;
+	
+	public ImprovedStackImpl(List list){
+		this.internalList = list;
+		
 	}
 	
 	/**
@@ -42,6 +45,31 @@ public class ImprovedStackImpl implements ImprovedStack {
 	 */
 	public void remove(Object object){
 		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public void push(Object item) {
+		
+	}
+
+	@Override
+	public ReturnObject top() {
+		return null;
+	}
+
+	@Override
+	public ReturnObject pop() {
+		return null;
 	}
 }
 
