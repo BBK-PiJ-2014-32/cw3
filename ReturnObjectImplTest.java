25d5b28ff7b9b91450069f616a8d1d2dca00cd10
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-27 22:59:13 +0000

    getObjectValueTest added and passed

--- ReturnObjectImplTest.java
+++ ReturnObjectImplTest.java
@@ -5,20 +5,18 @@ import org.junit.Test;
 public class ReturnObjectImplTest {
 	
 	@Test
-	public void emptyObjectTest(){
+	public void nullObjectTest(){
 		ReturnObject testObject = new ReturnObjectImpl(null);
 		ErrorMessage output = testObject.getError();
 		ErrorMessage expected = ErrorMessage.NO_ERROR;
 		assertEquals(expected, output);
 	}
 	
-	
-	/*public static void main(String[] args){
-		ReturnObject testObject = new ReturnObjectImpl(null);
-		ErrorMessage msg = testObject.getError();
-		System.out.println(msg);
-		ReturnObject testObject2 = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
-		ErrorMessage msg2 = testObject2.getError();
-		System.out.println(msg2);
-	}*/
+	@Test
+	public void getObjectValueTest(){
+		ReturnObject testObject = new ReturnObjectImpl("Testing!");
+		Object output = testObject.getReturnValue();
+		Object expected = "Testing!";
+		assertEquals(expected, output);
+	}
 }
