9e84d57eb2089eccce41197356e90165bcab1e71
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-27 22:55:23 +0000

    changed to JUnit test class

--- ReturnObjectImplTest.java
+++ ReturnObjectImplTest.java
@@ -1,11 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class ReturnObjectImplTest {
 	
-	public static void main(String[] args){
+	@Test
+	public void emptyObjectTest(){
+		ReturnObject testObject = new ReturnObjectImpl(null);
+		ErrorMessage output = testObject.getError();
+		ErrorMessage expected = ErrorMessage.NO_ERROR;
+		assertEquals(expected, output);
+	}
+	
+	
+	/*public static void main(String[] args){
 		ReturnObject testObject = new ReturnObjectImpl(null);
 		ErrorMessage msg = testObject.getError();
 		System.out.println(msg);
 		ReturnObject testObject2 = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 		ErrorMessage msg2 = testObject2.getError();
 		System.out.println(msg2);
-	}
+	}*/
 }
