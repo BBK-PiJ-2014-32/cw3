6230d926bb47b9df7dcbc19fa3a04dc71dbf889d
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-01 18:09:58 +0000

    fullTest adapted

--- FunctionalLinkedListTest.java
+++ FunctionalLinkedListTest.java
@@ -38,4 +38,32 @@ public class FunctionalLinkedListTest {
 		assertEquals(expected, output);
 	}
 
+	@Test
+	public void fullTest() {
+		FunctionalLinkedList myLinkedList = new FunctionalLinkedList();
+		myLinkedList.add("a");
+		myLinkedList.add("b");
+		myLinkedList.add("c");
+		myLinkedList.remove(2);
+		myLinkedList.add("d");
+		myLinkedList.add("e");
+		myLinkedList.remove(2);
+		myLinkedList.add("f");
+		myLinkedList.add(123);
+		myLinkedList.add(12);
+		myLinkedList.add("something");
+		myLinkedList.add(2);
+		myLinkedList.remove(2);
+		myLinkedList.add(187.12);
+		myLinkedList.add("A");
+		myLinkedList.add("B");
+		myLinkedList.add("C");
+		myLinkedList.add("D");
+		myLinkedList.add("E");
+		myLinkedList.remove(2);
+		FunctionalLinkedList newLinkedList = myLinkedList.rest();
+		System.out.println(myLinkedList.get(0).getReturnValue());
+		System.out.println(newLinkedList.get(0).getReturnValue());
+		
+	}
 }
